83f4f8b35fc6838697f9fbab0a8002cc9727f073
Transformatorishe <nafnafol345@gmail.com>
2020-01-05 12:46:31 +0300

    самый дружный друг

---
+++ popular-friend.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import time
+
+ffile=open('token.txt','r')
+token=ffile.read()
+ffile.close()
+url='https://api.vk.com/method/friends.get'
+
+def main(url, token, id):
+    r=requests.get(url=url, params={'user_id': id , 'v': '5.52', 'access_token': token})
+    friends=json.loads(r.text)['response']
+    return friends
+
+frcount=0
+fr=''
+b=main(url,token,'419858248')['items']
+for i in b:
+    k=main(url,token,str(i))['count']
+    if k>frcount:
+        frcount=k
+        fr=i
+    time.sleep(1)
+
+r=requests.get(url='https://api.vk.com/method/users.get', params={'user_id': fr ,
+'v': '5.52',
+'access_token': token})
+print(frcount)
+print(r.text)
